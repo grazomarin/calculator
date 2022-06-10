e8407e40e61fa0b3d9fe03a4c0843311e4289b95
Kamran <huseynovk355@gmail.com>
2022-06-10 22:16:56 +0400

    added remainder division and fixed the issue with multiple pressing of sign button

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let session = [];
 let clickCount = 0;
 let signCount = 0;
 let dotCount = 0;
-let divideCount = 0;
+let abuseCount = 0;
 
 function math () {
     pressedNumber();
@@ -23,6 +23,7 @@ function math () {
 function pressedNumber () {
     numbers.forEach(number => {
         number.addEventListener('click', () => {
+            abuseCount = 0;
             signs.forEach(sign => {sign.classList.remove('clicked')});
             if (clickCount === 0) {
                 bar.textContent = '';
@@ -39,17 +40,21 @@ function pressedSign () {
     signs.forEach(sign => {
         sign.addEventListener('click', () => {
             dotCount = 0
-            sign.classList.add('clicked')
-            if (signCount === 0) {
-                clickCount = 0;
-                session.push(Number(bar.textContent));
-                session.push(sign.textContent);
-                signCount += 1
-            } else {
-                session.push(Number(bar.textContent));
-                evaluate();
-                session.push(Number(bar.textContent));
-                session.push(sign.textContent);
+            signs.forEach(sign => sign.classList.remove('clicked')); // 
+            sign.classList.toggle('clicked')
+            if (abuseCount === 0) {
+                if (signCount === 0) {
+                    clickCount = 0;
+                    session.push(Number(bar.textContent));
+                    session.push(sign.textContent);
+                    signCount += 1
+                    abuseCount += 1
+                } else {
+                    session.push(Number(bar.textContent));
+                    evaluate();
+                    session.push(Number(bar.textContent));
+                    session.push(sign.textContent);
+                } 
             }
         })
     })
@@ -63,6 +68,7 @@ function evaluate() {
         else if(session[1] === '-') {result = minus(session[0], session[2])}
         else if(session[1] === '*') {result = multiply(session[0], session[2])}
         else if (session[1] === '/') {result = divide(session[0], session[2])}
+        else if (session[1] === '%') {result = remainder(session[0], session[2])}
         session = []
         bar.textContent = parseFloat(result.toFixed(3))
         if (bar.textContent == 'NaN' || bar.textContent == 'Infinity') {bar.textContent = 'ERROR';}
@@ -70,10 +76,10 @@ function evaluate() {
 
 
 equal.addEventListener('click', () => {
+    signs.forEach(sign => sign.classList.remove('clicked'));
     dotCount = 0
     session.push(Number(bar.textContent));
     evaluate();
-    //session.push(Number(bar.textContent));
     signCount = 0;
 });
 
@@ -81,18 +87,22 @@ function add (a, b) {return a + b};
 function minus (a, b) {return a - b};
 function multiply (a, b) {return a * b};
 function divide (a, b) {return a / b};
+function remainder (a, b) {return a % b}; //
 
 math();
 
 ac.addEventListener('click', () => {
+    signs.forEach(sign => sign.classList.remove('clicked')); 
     dotCount = 0
     signCount = 0;
     clickCount = 0;
+    abuseCount = 0;
     bar.textContent = '0';
     session = [];
 })
 
 dot.addEventListener('click', () => {
+    signs.forEach(sign => sign.classList.remove('clicked')); 
     if (dotCount === 0) {
         bar.textContent += '.';
         dotCount += 1;
@@ -100,6 +110,7 @@ dot.addEventListener('click', () => {
 });
 
 plusMinus.addEventListener('click', () => {
+    signs.forEach(sign => sign.classList.remove('clicked')); 
     const barAray = Array.from(bar.textContent)
     if (barAray[0] == '-') {
         barAray.splice(0,1);
@@ -108,13 +119,9 @@ plusMinus.addEventListener('click', () => {
         barAray.unshift('-');
         bar.textContent = `${barAray.join('')}`;
     }
-    
-
 });
 
-
-
-// fix the second equation is started without restart the calculator breaks
+// fix the problem with multiple times pressing plus minus
 
 /* numbers are pressed we are just pushing the values into the text content of result. If
 not a number is pressed we will record the value of the 1st part and accept the sign. afterwards
